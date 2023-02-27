cd99dc9283fd01cea7e63a13723803e4923b29f4
diazraelwang <446980095@qq.com>
2023-02-28 00:40:31 +0800

    deployserver build

--- README.md
+++
@@ -1,2 +0,0 @@
-# EasyWeb
-为了构建一个简单的DevOps流程，实现的Web源码项目。
--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 #
-kill -9 $(pgrep EasyWeb)
+kill -9 $(pgrep webserver)
 cd /tmp/EasyWeb
 git pull https://github.com/diazraelwang/EasyWeb.git
-./deployserver/deployserver &
+cd webserver/
+chmod 755 。/webserver
+./webserver &
